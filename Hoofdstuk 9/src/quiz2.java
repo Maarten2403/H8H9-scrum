dd2b79b5e1268ff7e5410b343a5e36eb5f9d17ce
zakurager12 <48752390+zakurager12@users.noreply.github.com>
2019-04-05 09:12:08 +0200

    Add files via upload

---
+++ Hoofdstuk 9/src/quiz2.java
@@ -0,0 +1,21 @@
+
+public class quiz2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = new int [5];
+		arr[0] = 3;
+		arr[1] = 7;
+		arr[4] = 3;
+		arr[3] = 1;
+		arr[1] = 8;
+		
+		for (int i = 0; i < arr.length; i ++) {
+			if(arr[i] >= 3 ) {
+			System.out.print(arr[i] + " ");
+			}
+		}
+		
+	}
+
+}
